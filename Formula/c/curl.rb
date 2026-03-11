6a8b86e71eeb94421243e4f0f6453aa3f9caedef
Sean Molenaar <SMillerDev@users.noreply.github.com>
2026-03-11 12:10:48 +0100

    Update curl.rb mirror URL for version 8.19.0

--- Formula/c/curl.rb
+++ Formula/c/curl.rb
@@ -3,7 +3,7 @@ class Curl < Formula
   homepage "https://curl.se"
   # Don't forget to update both instances of the version in the GitHub mirror URL.
   url "https://curl.se/download/curl-8.19.0.tar.bz2"
-  mirror "https://github.com/curl/curl/releases/download/curl-8_18_0/curl-8.19.0.tar.bz2"
+  mirror "https://github.com/curl/curl/releases/download/curl-8_19_0/curl-8.19.0.tar.bz2"
   mirror "http://fresh-center.net/linux/www/curl-8.19.0.tar.bz2"
   mirror "http://fresh-center.net/linux/www/legacy/curl-8.19.0.tar.bz2"
   sha256 "eba3230c1b659211a7afa0fbf475978cbf99c412e4d72d9aa92d020c460742d4"
